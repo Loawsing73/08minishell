8dd6b8484eb8aef75ec3cc371410e0f09f776c07
Loawz <44000088+Lowsing73@users.noreply.github.com>
2025-09-15 16:37:26 +0200

    oksdour

--- TRY_LEX/diff.c
+++ TRY_LEX/diff.c
@@ -1,6 +1,6 @@
 #include "token.h"
 
-t_token	get_token(char *s)
+t_token	get_token(char c)
 {
 	if (!s)
 		return (ERROR);
@@ -8,56 +8,72 @@ t_token	get_token(char *s)
 //		return (QUOTE);
 //	else if (s[0] == '"')
 //		return (DOUBLE_QUOTE);
-	else if (s[0] == '<')
+	else if (c == '<')
 	{	
 		if (s[1] == '<')
 			return (D_LESS);
 		return (LESS);
 	}
-	else if (s[0] == '>')
+	else if (c == '>')
 	{
 		if (s[1] == '>')
 			return (D_GREAT);
 		return (GREAT);
 	}
-	else if (s[0] == '|')
+	else if (c == '|')
 		return (PIPE);
-	else if (s[0] == '\n')
-		return (NL);
 	else
 		return (WORD);
 }
-t_cursor	switch_token(t_token token, t_cursor *cursor)
+//
+void	switch_token(t_token token, t_cursor *cursor)
 {
-	
+	cursor->prev = cursor->current;
+	cursor->current = token;
+}
+//
+t_lexem	create_node(t_lexem *head, t_token token, char *input)
+{
+	t_lexm	*node;
+
+	node = malloc(sizeof(t_lexem));
+	node->token = token;
+	node->input = input;
+	node->next = NULL;
+	if (head)
+	{
+		node->next = head;
+		head = node;
+		return (head);
+	}
+	return (node);
 }
 
-
-
+//
 	parsing_input(t_cursor *cursor, char *input)
 {
+	t_lexem	*head;
+
 	cursor->input = ft_strdup(intput);
 	cursor->position = 0;
 	cursor->current = ERROR;
 	while (cursor->input[cursor->position])
 	{
 		if (cursor->input[cursor->position] == '|')
-		{	
-			
-			creat_node(PIPE, ft_substr(cursor->input, cursor_poition, 1));
-		}
+			is_pipe(head, cursor);
 		else if (cursor->input[cursor->position] == '"')
-			creat_node(DOUBLE_QUOTE, ft_substr(cursor->input, cursor_poition, 1));
+			
 		else if (cursor->input[cursor->position] == '\'')
-			creat_node(DOUBLE_QUOTE, ft_substr(cursor->input, cursor_poition, 1));
+			
 		else if (cursor->input[cursor->position] == '>')
-			creat_node(GREAT, ft_substr(cursor->input, cursor_poition, 1));
-	else if (cursor->input[cursor->position] == '<')
-			creat_node(LESS, ft_substr(cursor->input, cursor_poition, 1));
+			
+		else if (cursor->input[cursor->position] == '<')
+			
+		current->position++;
 	}
 
 }	
-
+//
 
 
 
