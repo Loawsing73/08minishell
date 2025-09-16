015e0b1382ca8b34d3a103c21cc4275b5e3ca78d
Loawz <44000088+Lowsing73@users.noreply.github.com>
2025-09-16 13:16:51 +0200

    seg fault mais pasring fini, juste beaoin de fix

--- TRY_LEX/token.h
+++ TRY_LEX/token.h
@@ -17,12 +17,8 @@ typedef	enum e_token
 	GREAT,
 	NL,
 	PIPE,
-//	R_PARENT,
-//	L_PARENT,
-//	R_BRACKETS,
-//	L_BRACKETS,
-//	QUOTE,
-//	DOUBLE_QUOTE,
+	QUOTE,
+	DOUBLE_QUOTE,
 	ERROR
 }	t_token;
 
@@ -30,7 +26,7 @@ typedef struct	s_lexem
 {
 	t_token	token;
 	char	*input;
-	t_lexem	*next;
+	struct s_lexem	*next;
 }	t_lexem;
 typedef	struct	s_cursor
 {
@@ -43,7 +39,19 @@ typedef	struct	s_cursor
 char	*ft_substr(char *s, size_t start, size_t len);
 char	**ft_split(char const *s, char c);
 size_t	ft_strlen(char *s);
-ssize_t	get_index(char *s);
-char	**ft_split_pipe(char *s);
+char	*ft_strdup(char *s1);
+
+int	is_pipe(t_lexem *head, t_cursor *cursor);
+int	is_double_quote(t_lexem *head, t_cursor *cursor);
+int	is_word(t_lexem *head, t_cursor *cursor);
+int	is_great(t_lexem *head, t_cursor *cursor);
+int	is_less(t_lexem *head, t_cursor *cursor);
+int	is_quote(t_lexem *head, t_cursor *cursor);
+int	get_index(int position, char *s, char c);
+t_token	get_token(char c);
+void	switch_token(t_token token, t_cursor *cursor);
+t_lexem	*create_node(t_lexem *head, t_token token, char *input);
+t_lexem *parsing_input(t_cursor *cursor, char *input);
+
 
 #endif
