91bcfd8e1364087b3b8aa24b001d8f7de9a4383d
Loawz <44000088+Lowsing73@users.noreply.github.com>
2025-10-15 10:39:28 +0200

    pre clean avec mamer

--- PARSING/SRCS/tokenization.c
+++ PARSING/SRCS/tokenization.c
@@ -215,14 +215,14 @@ int main(int ac, char **ag, char **env)
 	char *input;
 	char *joined;
 	t_lexem *head;
-	//t_lexem *current;
+	t_lexem *current;
 	t_cursor cursor = {0};
 	
 	(void)ac;
 	(void)ag;
 	while (1)
 	{
-		input = readline("minishell >> ");
+		input = readline("minishell >>");
 		if (!input)
 		{
 			clear_history();
@@ -239,19 +239,19 @@ int main(int ac, char **ag, char **env)
 		if (valid_input(ft_strtrim(input, " ")))
 		{
 			joined = concate_hell(input, env);
-			// clean_neg_ascii(joined);
-			//printf("%s\n", joined);
+			 //clean_neg_ascii(joined);
+			printf("%s\n", joined);
 			head = parsing_input(&cursor, joined);
-			//current = head;
-			//while (current)
-			//{
-			//	printf("token = %u, input = %s\n", current->token, current->input);
-			//	current = current->next;
-			//}
+			current = head;
+			while (current)
+			{
+				printf("token = %u, input = %s\n", current->token, current->input);
+				current = current->next;
+			}
 			t_ast_node *ast = parse(head);
 			if (ast)
 			{
-			//	print_ast(ast, 0);
+				print_ast(ast, 0);
 				execute_ast(ast, env);
 				free_ast(ast);
 			}
