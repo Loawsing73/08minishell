457526f295c7aafdb47ddbe696c952d22dcf2f4b
Mathieu Meerbergen <mameerbe@fu-r9-p4.s19.be>
2025-11-21 00:17:58 +0100

    again

--- src/exec/execute_utils.c
+++ src/exec/execute_utils.c
@@ -1,24 +1,25 @@
 #include "../../includes/minishell.h"
 
-void    execute_pipeline_1(t_global *global, t_pipeline *data, int *i)
+void    execute_pipeline_1(t_global *global, t_pipeline *data)
 {
     data->command = global.tree->children[0];
     data->cmd_name = NULL;
-    i = 0;
-    while (i < data->command->child_count)
+    data->i = 0;
+    while (data->i < data->command->child_count)
     {
-        if (data->command->children[i]->type == NODE_WORD)
+        if (data->command->children[data->i]->type == NODE_WORD)
         {
-            data->cmd_name = data->command->children[i]->value;
+            data->cmd_name = data->command->children[data->i]->value;
             break ;
         }
-        i++;
+        data->i++;
     }
     if (data->cmd_name && is_builtins(data->cmd_name))
         data->is_single_builtin = 1;
 }
 
-void    execute_pipeline_2(t_global *global, t_pipeline *data, int i, int j, int k)
+void    execute_pipeline_2(t_global *global, t_pipeline *data)
 {
     data->arg_count = 0;
+    dq
 }
